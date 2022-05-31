ee2649cc36b4380f4536f835e152592c03b06505
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2022-05-31 23:49:27 +0500

    вынес константы

--- application/keys.py
+++ application/keys.py
@@ -1,8 +1,8 @@
-MENU_KEY = ("m", "M", 'ь', 'Ь')
+MENU_KEY = ("m", "M", "ь", "Ь")
 SPECIAL_SYMBOLS = (263, "BS", "KEY_BACKSPACE", "\b", "\x7f", 8)
 EXIT_KEY = 27
 KEY_O = ("O", "o", "Щ", "щ")
 KEY_N = ("N", "n", "т", "Т")
 KEY_S = ("S", "s", "С", "с")
 KEY_G = ("G", "g", "П", "п")
-KEY_R = ("R", 'r', "Р", "р")
+KEY_R = ("R", "r", "К", "к")
